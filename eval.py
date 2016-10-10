c0fdd2d5405f1375403a12f5bfae6a1263fe5589
Grant Van Horn <gvanhorn@visipedia.org>
2016-10-10 11:09:01 -0700

    first pass at adding evaluation code. Currently this uses the COCO evaluation code.

---
+++ eval.py
@@ -0,0 +1,303 @@
+"""
+Use the COCO evaluation pipeline.
+"""
+
+import argparse
+import copy
+import cPickle as pickle
+from cStringIO import StringIO
+import logging
+import numpy as np
+import pprint
+import sys
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import time
+
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
+from config import parse_config_file
+import eval_inputs as inputs
+import loss
+import model
+
+def eval(tfrecords, bbox_priors, summary_dir, checkpoint_path, max_iterations, cfg):
+  
+  logger = logging.getLogger()
+  logger.setLevel(logging.DEBUG)
+
+  graph = tf.Graph()
+
+  # Force all Variables to reside on the CPU.
+  with graph.as_default():
+
+    batched_images, batched_bboxes, batched_num_bboxes, batched_image_ids = inputs.input_nodes(
+      tfrecords=tfrecords,
+      max_num_bboxes = cfg.MAX_NUM_BBOXES,
+      num_epochs=None,
+      batch_size=cfg.BATCH_SIZE,
+      num_threads=cfg.NUM_INPUT_THREADS,
+      capacity=cfg.QUEUE_CAPACITY,
+      shuffle_batch=True,
+      cfg=cfg
+    )
+
+    batch_norm_params = {
+      # Decay for the batch_norm moving averages.
+      'decay': cfg.BATCHNORM_MOVING_AVERAGE_DECAY,
+      # epsilon to prevent 0s in variance.
+      'epsilon': 0.001,
+      'variables_collections' : [tf.GraphKeys.MOVING_AVERAGE_VARIABLES],
+      'is_training' : False
+    }
+    with slim.arg_scope([slim.conv2d],
+                        activation_fn=tf.nn.relu,
+                        normalizer_fn=slim.batch_norm,
+                        normalizer_params=batch_norm_params,
+                        weights_regularizer=slim.l2_regularizer(0.00004),
+                        biases_regularizer=slim.l2_regularizer(0.00004)):
+      
+      
+      locations, confidences, inception_vars = model.build(
+        inputs = batched_images,
+        num_bboxes_per_cell = cfg.NUM_BBOXES_PER_CELL,
+        reuse=False,
+        scope=''
+      )
+
+    ema = tf.train.ExponentialMovingAverage(
+      decay=cfg.MOVING_AVERAGE_DECAY
+    )   
+    shadow_vars = {
+      ema.average_name(var) : var
+      for var in slim.get_model_variables()
+    }
+  
+    
+    # Restore the parameters
+    saver = tf.train.Saver(shadow_vars, reshape=True)
+
+    fetches = [locations, confidences, batched_bboxes, batched_num_bboxes, batched_image_ids]
+    
+    coord = tf.train.Coordinator()
+
+    sess_config = tf.ConfigProto(
+      log_device_placement=False,
+      #device_filters = device_filters,
+      allow_soft_placement = True,
+      gpu_options = tf.GPUOptions(
+          per_process_gpu_memory_fraction=cfg.SESSION_CONFIG.PER_PROCESS_GPU_MEMORY_FRACTION
+      )
+    )
+    sess = tf.Session(graph=graph, config=sess_config)
+
+    dataset_image_ids = set()
+    gt_annotations = []
+    pred_annotations = [] # {imageID,x1,y1,w,h,score,class}
+    gt_annotation_id = 1
+
+    with sess.as_default():
+
+      tf.initialize_all_variables().run()
+      tf.initialize_local_variables().run()
+      threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+      try:
+
+        if tf.gfile.IsDirectory(checkpoint_path):
+          checkpoint_path = tf.train.latest_checkpoint(checkpoint_path)
+        
+        if checkpoint_path is None:
+          print "ERROR: No checkpoint file found."
+          return
+
+        # Restores from checkpoint
+        saver.restore(sess, checkpoint_path)
+        # Assuming model_checkpoint_path looks something like:
+        #   /my-favorite-path/cifar10_train/model.ckpt-0,
+        # extract global_step from it.
+        global_step = int(checkpoint_path.split('/')[-1].split('-')[-1])
+        print "Found model for global step: %d" % (global_step,)
+        
+        print_str = ', '.join([
+          'Step: %d',
+          'Time/image (ms): %.1f'
+        ])
+
+        step = 0
+        while not coord.should_stop():
+          
+          t = time.time()
+          outputs = sess.run(fetches)
+          dt = time.time()-t
+          
+          locs = outputs[0]
+          confs = outputs[1]
+          all_gt_bboxes = outputs[2]
+          all_gt_num_bboxes = outputs[3]
+          image_ids = outputs[4]
+
+          for b in range(cfg.BATCH_SIZE):
+
+            img_id = int(image_ids[b])
+            
+            predicted_bboxes = locs[b] + bbox_priors
+            predicted_bboxes = np.clip(predicted_bboxes, 0., 1.)
+            predicted_confs = confs[b]
+
+            gt_bboxes = all_gt_bboxes[b]
+            gt_num_bboxes = all_gt_num_bboxes[b]
+
+            # Scale the predictions and ground truth boxes
+            # GVH: Should check to see if we are preserving aspect ratio or not...
+            im_scale = np.array([cfg.INPUT_SIZE, cfg.INPUT_SIZE, cfg.INPUT_SIZE, cfg.INPUT_SIZE])
+            predicted_bboxes = predicted_bboxes * im_scale
+            gt_bboxes = gt_bboxes * im_scale
+          
+            
+            # Sort the predictions based on confidences
+            sorted_idxs = np.argsort(predicted_confs.ravel())[::-1]
+            sorted_bboxes = predicted_bboxes[sorted_idxs]
+            sorted_confs = predicted_confs[sorted_idxs]
+
+            # Store the results
+            for k in range(100):
+              x1, y1, x2, y2 = sorted_bboxes[k]
+              score = sorted_confs[k]
+              pred_annotations.append([
+                img_id,
+                x1, y1, x2 - x1, y2 - y1, 
+                score,
+                1
+              ])
+
+            for k in range(gt_num_bboxes):  
+              x1, y1, x2, y2 = gt_bboxes[k]
+              w = x2 - x1
+              h = y2 - y1
+              gt_annotations.append({
+                "id" : gt_annotation_id,
+                "image_id" : img_id,
+                "category_id" : 1,
+                "area" : w * h,
+                "bbox" : [x1, y1, w, h],
+                "iscrowd" : 0,
+              })
+              gt_annotation_id += 1
+
+            dataset_image_ids.add(img_id)
+
+          step += 1
+          print print_str % (step, (dt / cfg.BATCH_SIZE) * 1000)
+
+          if max_iterations > 0 and step == max_iterations:
+            break  
+
+      except tf.errors.OutOfRangeError as e:
+        pass
+        
+      coord.request_stop()
+      coord.join(threads)
+
+      pred_annotations = np.array(pred_annotations)
+      gt_dataset = {
+        'annotations' : gt_annotations,
+        'images' : [{'id' : img_id} for img_id in dataset_image_ids],
+        'categories' : [{ 'id' : 1 }]
+      }
+      gt_coco = COCO()
+      gt_coco.dataset = gt_dataset
+      gt_coco.createIndex()
+
+      pred_coco = gt_coco.loadRes(pred_annotations)
+
+      cocoEval = COCOeval(gt_coco, pred_coco, iouType='bbox')
+
+      cocoEval.params.useCats = 0
+      #cocoEval.params.areaRange = ("medium","large") # I just created a different gt annotation file
+      cocoEval.evaluate()
+      cocoEval.accumulate()
+
+      old_stdout = sys.stdout
+      sys.stdout = captured_stdout = StringIO()
+      cocoEval.summarize()
+      sys.stdout = old_stdout
+
+      summary_op = tf.merge_all_summaries()
+      summary_writer = tf.train.SummaryWriter(summary_dir)
+      summary = tf.Summary()
+      summary.ParseFromString(sess.run(summary_op))
+
+      for line in captured_stdout.getvalue().split('\n'):
+        if line != "":
+          description, score = line.rsplit("=", 1)
+          description = description.strip()
+          score = float(score)
+
+          summary.value.add(tag=description, simple_value=score)
+
+          print "%s: %0.3f" % (description, score)
+      
+      summary_writer.add_summary(summary, global_step)
+      summary_writer.flush()
+      summary_writer.close()
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description='Detect objects using a pretrained Multibox model')
+
+    parser.add_argument('--tfrecords', dest='tfrecords',
+                        help='paths to tfrecords files', type=str,
+                        nargs='+', required=True)
+    
+    parser.add_argument('--priors', dest='priors',
+                          help='path to the bounding box priors pickle file', type=str,
+                          required=True)
+    
+    parser.add_argument('--summary_dir', dest='summary_dir',
+                          help='path to directory to store summary files and checkpoint files', type=str,
+                          required=True)
+
+    parser.add_argument('--checkpoint_path', dest='checkpoint_path',
+                          help='Either a path to a specific model, or a path to a directory where checkpoint files are stored. If a directory, the latest model will be tested against.', type=str,
+                          required=True, default=None)
+
+    parser.add_argument('--config', dest='config_file',
+                        help='Path to the configuration file',
+                        required=True, type=str)
+    
+    parser.add_argument('--max_iterations', dest='max_iterations',
+                        help='Maximum number of iterations to run. Set to 0 to run on all records.',
+                        required=False, type=int, default=0)              
+
+    args = parser.parse_args()
+    
+    return args
+
+def main():
+  args = parse_args()
+  print "Command line arguments:"
+  pprint.pprint(vars(args))
+  print
+
+  cfg = parse_config_file(args.config_file)
+  print "Configurations:"
+  pprint.pprint(cfg)
+  print 
+    
+  with open(args.priors) as f:
+    bbox_priors = pickle.load(f)
+  bbox_priors = np.array(bbox_priors).astype(np.float32)
+  
+  eval(
+    tfrecords=args.tfrecords,
+    bbox_priors=bbox_priors,
+    summary_dir=args.summary_dir,
+    checkpoint_path=args.checkpoint_path,
+    max_iterations = args.max_iterations,
+    cfg=cfg
+  ) 
+
+if __name__ == '__main__':
+  main()
---
+++ eval_inputs.py
@@ -0,0 +1,305 @@
+"""
+Input pipeline for training the detector. 
+
+Some of the augmentation code came from the inception examples
+in the tensorflow repo, so I am including their license. 
+"""
+
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import numpy as np
+from scipy.misc import imresize
+import tensorflow as tf
+import sys 
+from tensorflow.python.ops import control_flow_ops
+
+def apply_with_random_selector(x, func, num_cases):
+  """Computes func(x, sel), with sel sampled from [0...num_cases-1].
+  Args:
+    x: input Tensor.
+    func: Python function to apply.
+    num_cases: Python int32, number of cases to sample sel from.
+  Returns:
+    The result of func(x, sel), where func receives the value of the
+    selector as a python integer, but sel is sampled dynamically.
+  """
+  sel = tf.random_uniform([], maxval=num_cases, dtype=tf.int32)
+  # Pass the real x only to one of the func calls.
+  return control_flow_ops.merge([
+      func(control_flow_ops.switch(x, tf.equal(sel, case))[1], case)
+      for case in range(num_cases)])[0]
+
+
+def distort_color(image, color_ordering=0, fast_mode=True, scope=None):
+  """Distort the color of a Tensor image.
+  Each color distortion is non-commutative and thus ordering of the color ops
+  matters. Ideally we would randomly permute the ordering of the color ops.
+  Rather then adding that level of complication, we select a distinct ordering
+  of color ops for each preprocessing thread.
+  Args:
+    image: 3-D Tensor containing single image in [0, 1].
+    color_ordering: Python int, a type of distortion (valid values: 0-3).
+    fast_mode: Avoids slower ops (random_hue and random_contrast)
+    scope: Optional scope for name_scope.
+  Returns:
+    3-D Tensor color-distorted image on range [0, 1]
+  Raises:
+    ValueError: if color_ordering not in [0, 3]
+  """
+  with tf.name_scope(scope, 'distort_color', [image]):
+    if fast_mode:
+      if color_ordering == 0:
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+      else:
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+    else:
+      if color_ordering == 0:
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+        image = tf.image.random_hue(image, max_delta=0.2)
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+      elif color_ordering == 1:
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+        image = tf.image.random_hue(image, max_delta=0.2)
+      elif color_ordering == 2:
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+        image = tf.image.random_hue(image, max_delta=0.2)
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+      elif color_ordering == 3:
+        image = tf.image.random_hue(image, max_delta=0.2)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+      else:
+        raise ValueError('color_ordering must be in [0, 3]')
+
+    # The random_* ops do not necessarily clamp.
+    return tf.clip_by_value(image, 0.0, 1.0)
+
+def distorted_bounding_box_crop(image, image_height, image_width, 
+  xmin, ymin, xmax, ymax, num_bboxes,
+  min_object_covered = 0.7,
+  aspect_ratio_range = (0.7, 1.4),
+  area_range = (0.5, 1.0),
+  max_attempts = 100,
+  minimum_area = 50
+):
+    
+  # combine the bounding boxes (the shape should be [bbox_coords, num_bboxes])
+  bboxes = tf.concat(0, [ymin, xmin, ymax, xmax])
+  # order the bboxes so that they have the shape: [num_bboxes, bbox_coords]
+  bboxes = tf.transpose(bboxes, [1, 0])
+
+  sample_distorted_bounding_box = tf.image.sample_distorted_bounding_box(
+    tf.shape(image),
+    bounding_boxes=tf.expand_dims(bboxes, 0),
+    min_object_covered=min_object_covered,
+    aspect_ratio_range=aspect_ratio_range,
+    area_range=area_range,
+    max_attempts=max_attempts,
+    use_image_if_no_bounding_boxes=True
+  )
+  bbox_begin, bbox_size, distort_bbox = sample_distorted_bounding_box
+
+  # Crop the image to the specified bounding box.
+  cropped_image = tf.slice(image, bbox_begin, bbox_size)
+
+  # offset the ground truth bounding boxes 
+  bbox_begin = tf.cast(tf.slice(bbox_begin, [0], [2]), tf.float32)
+  bbox_size = tf.cast(tf.slice(bbox_size, [0], [2]), tf.float32)
+  
+  image_width = tf.cast(image_width, tf.float32)
+  image_height = tf.cast(image_height, tf.float32)
+  
+  cropped_bbox_ymin = tf.slice(bbox_begin, [0], [1])
+  cropped_bbox_xmin = tf.slice(bbox_begin, [1], [1])
+  bbox_end = bbox_begin + bbox_size
+  cropped_bbox_ymax = tf.slice(bbox_end, [0], [1])
+  cropped_bbox_xmax = tf.slice(bbox_end, [1], [1])
+  
+  scaled_ymin = ymin * image_height
+  scaled_ymax = ymax * image_height
+  scaled_xmin = xmin * image_width
+  scaled_xmax = xmax * image_width
+  
+  scaled_ymin = tf.maximum(scaled_ymin, tf.tile(cropped_bbox_ymin, [num_bboxes])) - cropped_bbox_ymin
+  scaled_xmin = tf.maximum(scaled_xmin, tf.tile(cropped_bbox_xmin, [num_bboxes])) - cropped_bbox_xmin
+  scaled_ymax = tf.minimum(scaled_ymax, tf.tile(cropped_bbox_ymax, [num_bboxes])) - cropped_bbox_ymin
+  scaled_xmax = tf.minimum(scaled_xmax, tf.tile(cropped_bbox_xmax, [num_bboxes])) - cropped_bbox_xmin
+  
+  scaled_ymin = tf.clip_by_value(scaled_ymin, 0.0, image_height)
+  scaled_xmin = tf.clip_by_value(scaled_xmin, 0.0, image_width)
+  scaled_ymax = tf.clip_by_value(scaled_ymax, 0.0, image_height)
+  scaled_xmax = tf.clip_by_value(scaled_xmax, 0.0, image_width)
+
+  scaled_areas = (scaled_xmax - scaled_xmin) * (scaled_ymax - scaled_ymin)
+  valid_areas = tf.cast(scaled_areas > minimum_area, tf.int32) 
+  
+  num_bboxes = tf.reduce_sum(valid_areas)
+  
+  _, scaled_ymin = tf.dynamic_partition(scaled_ymin, valid_areas, 2)
+  _, scaled_xmin = tf.dynamic_partition(scaled_xmin, valid_areas, 2)
+  _, scaled_ymax = tf.dynamic_partition(scaled_ymax, valid_areas, 2)
+  _, scaled_xmax = tf.dynamic_partition(scaled_xmax, valid_areas, 2)
+  
+  scaled_ymin = tf.expand_dims(scaled_ymin, 0) 
+  scaled_xmin = tf.expand_dims(scaled_xmin, 0)
+  scaled_ymax = tf.expand_dims(scaled_ymax, 0)
+  scaled_xmax = tf.expand_dims(scaled_xmax, 0)
+  
+  bbox_height = tf.cast(tf.slice(bbox_size, [0], [1]), tf.float32)
+  bbox_width = tf.cast(tf.slice(bbox_size, [1], [1]), tf.float32)
+  
+  xmin = scaled_xmin / bbox_width
+  ymin = scaled_ymin / bbox_height
+  xmax = scaled_xmax / bbox_width
+  ymax = scaled_ymax / bbox_height
+
+  return  cropped_image, xmin, ymin, xmax, ymax, num_bboxes  
+
+def distorted_shifted_bounding_box(xmin, ymin, xmax, ymax, num_bboxes, image_height, image_width, max_num_pixels_to_shift = 5):
+
+  image_width = tf.cast(image_width, tf.float32)
+  image_height = tf.cast(image_height, tf.float32)
+  one_pixel_width = 1. / image_width
+  one_pixel_height = 1. / image_height
+  max_width_shift = one_pixel_width * max_num_pixels_to_shift
+  max_height_shift = one_pixel_height * max_num_pixels_to_shift
+  
+  xmin -= tf.random_uniform([1, num_bboxes], minval=0, maxval=max_width_shift, dtype=tf.float32)
+  xmax += tf.random_uniform([1, num_bboxes], minval=0, maxval=max_width_shift, dtype=tf.float32)
+  ymin -= tf.random_uniform([1, num_bboxes], minval=0, maxval=max_height_shift, dtype=tf.float32)
+  ymax += tf.random_uniform([1, num_bboxes], minval=0, maxval=max_height_shift, dtype=tf.float32)
+  
+  # ensure that the coordinates are still valid
+  ymin = tf.clip_by_value(ymin, 0.0, 1.)
+  xmin = tf.clip_by_value(xmin, 0.0, 1.)
+  ymax = tf.clip_by_value(ymax, 0.0, 1.)
+  xmax = tf.clip_by_value(xmax, 0.0, 1.)
+  
+  return xmin, ymin, xmax, ymax
+
+def input_nodes(
+  
+  tfrecords, 
+
+  max_num_bboxes,
+
+  # number of times to read the tfrecords
+  num_epochs=1,
+
+  # Data queue feeding the model
+  batch_size=32,
+  num_threads=2,
+  shuffle_batch = True,
+  capacity = 1000,
+
+  # Global configuration
+  cfg=None):
+
+  with tf.name_scope('inputs'):
+
+    # A producer to generate tfrecord file paths
+    filename_queue = tf.train.string_input_producer(
+      tfrecords,
+      num_epochs=num_epochs
+    )
+
+    # Construct a Reader to read examples from the tfrecords file
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+
+    # Parse an Example to access the Features
+    features = tf.parse_single_example(
+      serialized_example,
+      features = {
+        'image/id' : tf.FixedLenFeature([], tf.string),
+        'image/encoded'  : tf.FixedLenFeature([], tf.string),
+        'image/height' : tf.FixedLenFeature([], tf.int64),
+        'image/width' : tf.FixedLenFeature([], tf.int64),
+        'image/object/bbox/xmin' : tf.VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/ymin' : tf.VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/xmax' : tf.VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/ymax' : tf.VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/count' : tf.FixedLenFeature([], tf.int64)
+      }
+    )
+
+    # Read in a jpeg image
+    image = tf.image.decode_jpeg(features['image/encoded'], channels=3)
+    
+    # Convert the pixel values to be in the range [0,1]
+    if image.dtype != tf.float32:
+      image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+
+    image_height = features['image/height']
+    image_width = features['image/width']
+    
+    image_id = features['image/id']
+
+    xmin = tf.expand_dims(features['image/object/bbox/xmin'].values, 0)
+    ymin = tf.expand_dims(features['image/object/bbox/ymin'].values, 0)
+    xmax = tf.expand_dims(features['image/object/bbox/xmax'].values, 0)
+    ymax = tf.expand_dims(features['image/object/bbox/ymax'].values, 0)
+    
+    num_bboxes = tf.cast(features['image/object/bbox/count'], tf.int32)
+    no_bboxes = tf.equal(num_bboxes, 0)
+
+    image = tf.image.resize_bilinear(tf.expand_dims(image, 0), [cfg.INPUT_SIZE, cfg.INPUT_SIZE], align_corners=False)
+    image = tf.squeeze(image)
+    
+    # combine the bounding boxes (the shape should be [bbox_coords, num_bboxes])
+    bboxes = tf.concat(0, [xmin, ymin, xmax, ymax])
+    # order the bboxes so that they have the shape: [num_bboxes, bbox_coords]
+    bboxes = tf.transpose(bboxes, [1, 0])
+    
+    # pad the number of boxes so that all images have `max_num_bboxes`
+    num_rows_to_pad = tf.maximum(0, max_num_bboxes - num_bboxes)   
+    bboxes = tf.cond(no_bboxes, lambda: tf.zeros([max_num_bboxes, 4]), lambda: tf.pad(bboxes, tf.pack([tf.pack([0, num_rows_to_pad]), [0, 0]])))
+    bboxes.set_shape([max_num_bboxes, 4])
+
+    image = tf.sub(image, 0.5)
+    image = tf.mul(image, 2.0)
+
+    if shuffle_batch:
+      images, batched_bboxes, batched_num_bboxes, image_ids = tf.train.shuffle_batch(
+        [image, bboxes, num_bboxes, image_id],
+        batch_size=batch_size,
+        num_threads=num_threads,
+        capacity= capacity, #batch_size * (num_threads + 2),
+        # Ensures a minimum amount of shuffling of examples.
+        min_after_dequeue= 0, # 3 * batch_size,
+        seed = cfg.RANDOM_SEED,
+      )
+
+    else:
+      images, batched_bboxes, batched_num_bboxes, image_ids = tf.train.batch(
+        [image, bboxes, num_bboxes, image_id],
+        batch_size=batch_size,
+        num_threads=num_threads,
+        capacity= capacity, #batch_size * (num_threads + 2),
+        enqueue_many=False
+      )
+    
+    return images, batched_bboxes, batched_num_bboxes, image_ids
+    
+    
+  
